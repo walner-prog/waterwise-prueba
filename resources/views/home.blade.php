b859418f59b7012fc2647b1e022327bc4c206da2
walner <git@walner.cl>
2024-09-06 20:54:52 -0600

    Primer commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-" crossorigin="anonymous" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-" crossorigin="anonymous" />
+         <!-- Agrega esto en la sección head de tu HTML -->
+           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" integrity="sha512-TCJ6FYD6dMj4wsiWZz6swnVMqB5RW2MaebusGM1h8zE3DlX5C4sG5ndooMU2t7pLzYl5GmMKa9oB/njpy5Ul9w==" crossorigin="anonymous" />
+              <!-- Otros encabezados -->
+    
+    @section('css')
+    <link rel="stylesheet" href="{{ asset('css/admin_custom.css') }}">
+      <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css">
+    
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+      <link rel="stylesheet" href="https://cdn.datatables.net/select/1.4.0/css/select.dataTables.min.css">
+     
+      @livewireStyles
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.bootstrap5.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.bootstrap5.css">
+  
+  @stop
+      <!-- Otros elementos del encabezado... -->
+      <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+      <head>
+        <script>
+          (function() {
+            const currentTheme = localStorage.getItem('theme');
+            if (currentTheme === 'dark') {
+              document.documentElement.classList.add('dark-mode');
+              document.documentElement.classList.remove('light-mode');
+            } else if (currentTheme === 'light') {
+              document.documentElement.classList.add('light-mode');
+              document.documentElement.classList.remove('dark-mode');
+            } else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+              document.documentElement.classList.add('dark-mode');
+            } else {
+              document.documentElement.classList.add('light-mode');
+            }
+          })();
+        </script>
+        <!-- Resto de tu <head> -->
+      </head>
+  </head>
+<body>
+    @extends('adminlte::page')
+
+@section('title', 'waterwise')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+@section('preloader')
+    <i class="fas fa-4x fa-spin fa-spinner text-secondary"></i>
+    <h4 class="mt-4 text-dark">Loading</h4>
+@stop
+  
+    
+@stop
+
+@section('css')
+
+
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@stop
+
+@section('js')
+
+@stop
+    
+</body>
+</html>
+
